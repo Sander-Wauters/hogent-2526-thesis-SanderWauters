4cd877eedffed283deb259ec9d02cc6c16c0a5aa
Sander-Wauters <sander1352@hotmail.be>
2025-11-05 11:46:44 +0100

    Add step 6-10 to poc

--- poc/updater/src/version/update16-17.ts
+++ poc/updater/src/version/update16-17.ts
@@ -56,7 +56,7 @@ function step5(project: Project): StepData {
 
 const metrics: StepData[] = [];
 metrics.push({
-  // 1
+  // 1 - CLI operation.
   detection: Capability.NOT,
   automation: Capability.FULLY,
   changeFlags: Change.IN_JSON | Change.IN_JSON,
@@ -64,7 +64,7 @@ metrics.push({
     "Make sure that you are using a supported version of node.js before you upgrade your application. Angular v17 supports node.js versions: v18.13.0 and newer",
 });
 metrics.push({
-  // 2
+  // 2 - CLI operation.
   detection: Capability.NOT,
   automation: Capability.FULLY,
   changeFlags: Change.IN_JSON | Change.IN_CLI,
@@ -72,7 +72,7 @@ metrics.push({
     "Make sure that you are using a supported version of TypeScript before you upgrade your application. Angular v17 supports TypeScript version 5.2 or later.",
 });
 metrics.push({
-  // 3
+  // 3 - CLI operation.
   detection: Capability.NOT,
   automation: Capability.FULLY,
   changeFlags: Change.IN_JSON | Change.IN_CLI,
@@ -80,7 +80,7 @@ metrics.push({
     "Make sure that you are using a supported version of Zone.js before you upgrade your application. Angular v17 supports Zone.js version 0.14.x or later.",
 });
 metrics.push({
-  // 4
+  // 4 - CLI operation.
   detection: Capability.NOT,
   automation: Capability.FULLY,
   changeFlags: Change.IN_CLI,
@@ -88,6 +88,46 @@ metrics.push({
     "In the application's project directory, run ng update @angular/core@17 @angular/cli@17 to update your application to Angular v17.",
 });
 metrics.push(step5(project));
+metrics.push({
+  // 6 - Config is specific to each project.
+  detection: Capability.NOT,
+  automation: Capability.NOT,
+  changeFlags: Change.IN_TYPESCRIPT | Change.TO_SYNTAX,
+  description:
+    "Make sure you configure setupTestingRouter, canceledNavigationResolution, paramsInheritanceStrategy, titleStrategy, urlUpdateStrategy, urlHandlingStrategy, and malformedUriErrorHandler in provideRouter or RouterModule.forRoot since these properties are now not part of the Router's public API",
+});
+metrics.push({
+  // 7 - Logic is specific to how the components are used. To many variables.
+  detection: Capability.NOT,
+  automation: Capability.NOT,
+  changeFlags: Change.IN_TYPESCRIPT | Change.IN_TEST | Change.TO_SEMANTICS,
+  description:
+    "For dynamically instantiated components we now execute ngDoCheck during change detection if the component is marked as dirty. You may need to update your tests or logic within ngDoCheck for dynamically instantiated components.",
+});
+metrics.push({
+  // 8 - Logic is specific to how errors are handled. To many variables.
+  detection: Capability.NOT,
+  automation: Capability.NOT,
+  changeFlags: Change.IN_TYPESCRIPT | Change.TO_SYNTAX | Change.TO_SEMANTICS,
+  description:
+    "Handle URL parsing errors in the UrlSerializer.parse instead of malformedUriErrorHandler because it's now part of the public API surface.",
+});
+metrics.push({
+  // 9 - Can't reproduce. Should be easily fixable.
+  detection: Capability.FULLY,
+  automation: Capability.FULLY,
+  changeFlags: Change.IN_TYPESCRIPT | Change.NOT_APPLICABLE | Change.TO_SYNTAX,
+  description:
+    "Change Zone.js deep imports like zone.js/bundles/zone-testing.js and zone.js/dist/zone to zone.js and zone.js/testing.",
+});
+metrics.push({
+  // 10 - Logic is specif to each redirect. To many variables.
+  detection: Capability.NOT,
+  automation: Capability.NOT,
+  changeFlags: Change.IN_TYPESCRIPT | Change.TO_SEMANTICS,
+  description:
+    "You may need to adjust your router configuration to prevent infinite redirects after absolute redirects. In v17 we no longer prevent additional redirects after absolute redirects.",
+});
 
 logStepData(metrics);
 
